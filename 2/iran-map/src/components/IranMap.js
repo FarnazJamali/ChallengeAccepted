0041e941b921cd85e2445476801c521d093b5a2f
FarnazJamali <fz.jamali@gmail.com>
2023-01-15 11:37:29 +0330

    [MODIFIED] Fetched data

--- 2/iran-map/src/components/IranMap.js
+++ 2/iran-map/src/components/IranMap.js
@@ -1,56 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import './IranMap.css';
+import "./IranMap.css";
 
-import CityModal from './CityModal';
-
+import CityModal from "./CityModal";
 
 class IranMap extends React.Component {
+  state = {
+    citiesData: null,
+    selectedCity: null,
+    isModalOpen: false,
+  };
 
-    state = {
-        citiesData: null,
-        selectedCity: null,
-        isModalOpen: false,
-    };
+  componentDidMount() {
+    const citiesData = this.state.citiesData;
+    fetch("http://localhost:9000/cities/")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({ citiesData: json });
+      });
+  }
+  cityClicked = (id) => (event) => {
+    event.preventDefault();
+    let selectedCity = this.state.selectedCity;
+    let isModalOpen = true;
 
+    const { data: cities } = fetch(`http://localhost:9000/cities/${id}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
 
-    cityClicked = (id) => (event) => {
-        event.preventDefault();
-        // Fetch city details and open modal
-    };
+    console.log(id);
+    selectedCity = id;
+    this.setState({ selectedCity, isModalOpen });
 
-    closeModal = () => {
-        this.setState({
-            isModalOpen: false,
-        });
-    };
+    // Fetch city details and open modal
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false,
+    });
+  };
 
     render() {
-        return (
-            <div>
-                <div className="map-container">
-                    {(this.state.citiesData || []).map((record) => (
-                        <div
-                            key={record.id}
-                            className="city-name"
-                            style={{
-                                top: `${record.top}%`,
-                                left: `${record.left}%`,
-                            }}
-                            onClick={this.cityClicked(record.id)}
-                        >
-                            {record.name}
-                        </div>
-                    ))}
-                </div>
-                <CityModal
-                    city={this.state.selectedCity}
-                    isOpen={this.state.isModalOpen}
-                    onClose={this.closeModal}
-                />
+    return (
+      <div>
+        <div className="map-container">
+          {(this.state.citiesData || []).map((record) => (
+            <div
+              key={record.id}
+              className="city-name"
+              style={{
+                top: `${record.top}%`,
+                left: `${record.left}%`,
+              }}
+              onClick={this.cityClicked(record.id)}
+            >
+              {record.name}
             </div>
-        );
-    }
+          ))}
+        </div>
+        <CityModal
+          city={this.state.citiesData}
+          isOpen={this.state.isModalOpen}
+          onClose={this.closeModal}
+        />
+      </div>
+    );
+  }
 }
 
 export default IranMap;
